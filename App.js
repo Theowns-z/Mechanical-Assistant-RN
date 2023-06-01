6c095af951150c2bd681afb83d8b7bf82c9a2f48
Daniel <danielmmd24@gmail.com>
2023-05-31 20:18:48 -0500

    terminar modelo

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import {  View, } from 'react-native';
+import {  View, ImageBackground } from 'react-native';
 import React from 'react';
 import Login from './Components/Login';
 
 export default function App() {
-  return (<Login/>);
+  return (
+    
+    
+    <Login/>
+    
+    
+  
+  );
 }
 
 
--- Components/Login.jsx
+++ Components/Login.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { View, Text, TextInput, Image ,StyleSheet, ImageBackground } from 'react-native';
 import Svg, { Path } from "react-native-svg"
 import SvgComponent from './SvgComponent';
-
+import{imagen} from '../Images/reparaciones.jpg';
 
 
 const Login = () => {
   return (
       
-       
+      
       <View style={styles.container}>
-      <Text style = {styles.titulo}>Bienvenido</Text>
-      <Text style = {styles.subtitulo}>Ingresa a tu cuenta</Text>
-      <TextInput style = {styles.TextInput} placeholder="Ingresar Correo Electronico "></TextInput> 
-      <TextInput style = {styles.TextInput} placeholder='Ingresar Contraseña'></TextInput>
+        <View style={styles.contedorimage}>
+          <SvgComponent style={styles.imageBackground} />
+        </View>
+        <Text style = {styles.titulo}>Bienvenido</Text>
+        <Text style = {styles.subtitulo}>Ingresa a tu cuenta</Text>
+        <Text style = {styles.subtitulo2}>Usuario</Text>
+        <TextInput style = {styles.TextInput} placeholder="Ingresar Correo Electronico "></TextInput> 
+        <Text style = {styles.subtitulo2}>Contraseña</Text>
+        <TextInput style = {styles.TextInput} placeholder='Ingresar Contraseña'></TextInput>
       </View>
       
+     
     
   );
 
@@ -25,20 +31,27 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#f1f1f1',
+      width: '100%',
+      
+      
+    },
+
+    contedorimage: {
+
       alignItems: 'center',
-      justifyContent: 'center',
+      
+
     },
 
     imageBackground: {
         
-        width : '50%',
-        height : '50%',
+        alignItems: 'center',
       },
   
     titulo: {
     
         fontSize: 60,
-        color: '#34434D',
+        color: '#276E90',
         fontWeight: 'bold',
         textAlign: 'center',
   
@@ -52,16 +65,29 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
 
+    subtitulo2: {
+
+      fontSize:15,
+      color: '#276E90',
+      fontWeight: 'bold',     
+      textAlign: 'left',
+      marginTop: 10,
+      paddingStart: 45,
+  },
+
     TextInput: {
         padding: 10,
         paddingStart: 20,
         borderWidth: 1,
-        borderColor: 'gray',
+        backgroundColor: '#276E90',
+        borderColor: '#F2AA1F',
         width: '80%',
         height: 40,
-        borderRadius: 10,
+        borderRadius: 20,
+        Textcolor: 'gray',
         marginTop: 10,
         backgroundColor: 'white',
+        alignSelf: 'center',
 
 
     }
